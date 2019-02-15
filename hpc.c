76fc126956e0b55bf0060582e4da30cfe8cab217
Philippe Roubert <philippe@roubert.email>
2019-02-15 19:17:30 +0000

    bug fix

--- hpc.c
+++ hpc.c
@@ -4,8 +4,8 @@
 double calculate_temperature(int i, int j, double temperatureArray[][500]){
     float deltaXY = 0.0002;
     float alphasquare = 0.01;
-    double spacial_derivativeX = (temperatureArray[i+1][j] - 2 * temperatureArray[i][j] + temperatureArray[i-1][j]) / ( powf(deltaXY,2));
-    double spacial_derivativeY = (temperatureArray[i][j+1] - 2 * temperatureArray[i][j] + temperatureArray[i][j-1]) / ( powf(deltaXY,2));
+    double spacial_derivativeX = (temperatureArray[i+1][j] - 2 * temperatureArray[i][j] + temperatureArray[i-1][j]) / ( deltaXY * deltaXY);
+    double spacial_derivativeY = (temperatureArray[i][j+1] - 2 * temperatureArray[i][j] + temperatureArray[i][j-1]) / ( deltaXY * deltaXY);
     double temperature = alphasquare * (spacial_derivativeX + spacial_derivativeY);
     return temperature;
 }
@@ -29,17 +29,13 @@ int main(void) {
   double timestepValue = 0.0000009;
   
   while(timestep < 100){
-    printf("Hello");
     timestep++;
-    for (i=0; i<500; i++){
-      for (j=0; j<500; j++){
+    for (i=1; i<499; i++){
+      for (j=1; j<499; j++){
         temperatures[i][j] += (calculate_temperature(i, j, temperatures) * timestepValue );
       }
     }
   }
-  printf("Hello World\n");
-  printf("%f", calculate_temperature(149, 149, temperatures));
-
   printf("Writing out results\n");
   FILE *outfile;
   outfile=fopen("hte.dat","w");
